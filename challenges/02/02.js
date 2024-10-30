388e7e968d6f57e4450eabd604e13ffb1a090ee2
Gabriel Bustamante <gabrieljuniorpb990@gmail.com>
2024-10-30 11:44:06 -0400

    update: challenge 02

--- challenges/02/02.js
+++ challenges/02/02.js
@@ -1,27 +1,19 @@
 function battleHorde(zombies, humans) {
-  const n = zombies.length;
+  let n = zombies.length;
   let humanPower = 0, zombiePower = 0;
 
   for(let i = 0; i < n; i++) {
     humanPower += humans[i] - '0';
     zombiePower += zombies[i] - '0';
-    if(humanPower > zombiePower) {
-      humanPower -= zombiePower;
-      zombiePower = 0;
-      continue;
-    }
 
-    if(zombiePower > humanPower) {
-      zombiePower -= humanPower;
-      humanPower = 0;
-      continue;
-    }
-
-    humanPower = zombiePower = 0;
+    let remainingHumanPower = Math.max(humanPower - zombiePower, 0);
+    let remainingZombiePower = Math.max(zombiePower - humanPower, 0);
+    humanPower = remainingHumanPower, zombiePower = remainingZombiePower;
   }
-  
-  if(zombiePower == humanPower) return "x";
-  return humanPower > zombiePower ? `${humanPower}h` : `${zombiePower}z`;
+
+  if(humanPower > zombiePower) return `${humanPower}h`
+  if(zombiePower > humanPower) return `${zombiePower}z`
+  return "x";
 }
 
 module.exports = battleHorde;
