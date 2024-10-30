7c21332fd72a77c156b8d7227029c49115420b46
Gabriel Bustamante <gabrieljuniorpb990@gmail.com>
2024-10-30 11:44:15 -0400

    update: challenge 03

--- challenges/03/03.js
+++ challenges/03/03.js
@@ -1,25 +1,18 @@
 function findSafestPath(dream) {
   let n = dream.length;
   let m = dream[0].length;
-  
   let dp = [];
-  for(let i = 0; i < n + 1; i++) {
-    dp.push(new Array(m + 1).fill(123456789));
+  for(let i = 0; i<=n; i++) {
+    dp.push(new Array(m+1).fill(123456789));
   }
-  dp[0][0] = dream[0][0];
-
-  for(let i = 0; i < n; i++) {
-    for(let j = 0; j < m; j++) {
-      if(i > 0) {
-        dp[i][j] = Math.min(dp[i][j], dream[i][j] + dp[i - 1][j]);
-      }
-      if(j > 0) {
-        dp[i][j] = Math.min(dp[i][j], dream[i][j] + dp[i][j - 1]);
-      }
+  dp[1][1] = dream[0][0];
+  for(let i = 1; i <= n; i++) {
+    for(let j = 1; j <= m; j++) {
+      dp[i][j] = Math.min(dp[i][j], dream[i-1][j-1] + dp[i - 1][j]);
+      dp[i][j] = Math.min(dp[i][j], dream[i-1][j-1] + dp[i][j - 1]);
     }
   }
-
-  return dp[n - 1][m - 1];
+  return dp[n][m];
 }
 
 module.exports = findSafestPath;
